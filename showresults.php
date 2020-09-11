0f7912abbc428c2367e6878bbebe6dc3d286369f
theotherdy <damion@allotmentor.com>
2020-09-11 20:28:56 +0100

    debugging criteria

--- showresults.php
+++ showresults.php
@@ -185,7 +185,8 @@ function getElementsByAttributeValue(tagName, attrName, attrValue) {
 					{
 					$iParticipantItems = "	INSERT INTO ParticipantItems
 											VALUES(0,".$rowParticipants['heraldID'].",".$QuestionToAnalyseByBlockID.",".$QuestionToAnalyseBySectionID.",".$QuestionToAnalyseByQuestionID.",".$QuestionToAnalyseByItem.",".$rowParticipants['instance'].",".$rowParticipants['sinstance'].")";
-					$result_query = @mysqli_query($db_connection, $iParticipantItems);
+					//$result_query = @mysqli_query($db_connection, $iParticipantItems);
+					$result_query = mysqli_query($db_connection, $iParticipantItems);
 					print_r($iParticipantItems);
 					print_r($result_query);
 					if (($result_query == false) && mysqli_affected_rows($db_connection) == 0)
